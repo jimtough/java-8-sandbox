073011fa63da2cf7816b9ad03750ade2744c4a9e
jtough <jtoughgithub@gmail.com>
2016-12-19 12:34:48 -0400

    Mavenized empty project

---
+++ src/test/java/com/jimtough/AppTest.java
@@ -0,0 +1,14 @@
+package com.jimtough;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		App.main("");
+		assertTrue(true);
+	}
+}
