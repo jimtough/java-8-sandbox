41fa89a18a0720757442ec9d2c4da244a97a7d13
jtough <jtoughgithub@gmail.com>
2018-05-28 18:49:53 -0300

    Added another test to ScannerToTokenizeTextFileTest class

--- src/test/java/com/jimtough/ch09/ScannerToTokenizeTextFileTest.java
+++ src/test/java/com/jimtough/ch09/ScannerToTokenizeTextFileTest.java
@@ -5,9 +5,18 @@ import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -32,7 +41,7 @@ public class ScannerToTokenizeTextFileTest {
 	//--------------------------------------------------------------------
 	
 	@Test
-	public void testScannerTokenizesTextFile() throws Exception {
+	public void testScannerTokenizesTextFile_A() throws Exception {
 		TreeMap<String,AtomicInteger> tokenToCountMap = new TreeMap<>();
 		try (
 				FileReader fr = new FileReader(BIG_SAMPLE_TEXT_FILE_PATH_A.toString());
@@ -72,5 +81,54 @@ public class ScannerToTokenizeTextFileTest {
 			// log them!
 			.forEach((s)->LOGGER.debug("{}", s));
 	}
+	
+	@Test
+	public void testScannerTokenizesTextFile_B() throws Exception {
+		LOGGER.debug("Reading contents of text file, tokenizing and loading into a List...");
+		List<String> listOfTokens = new ArrayList<>();
+		try (
+			FileReader fr = new FileReader(BIG_SAMPLE_TEXT_FILE_PATH_A.toString());
+			BufferedReader br = new BufferedReader(fr);
+			Scanner scanner = new Scanner(br);
+		) {
+			// This delimiter regex will match 'non-words'
+			scanner.useDelimiter("\\W");
+			while (scanner.hasNext()) {
+				String token = scanner.next();
+				if (!token.isEmpty()) {
+					listOfTokens.add(token);
+				}
+			}
+		}
+		assertFalse(listOfTokens.isEmpty());
+		LOGGER.debug("Contents of text file tokenized and loaded into a List");
+		
+		final Map<String,Long> tokenToCountMap;
+		try (Stream<String> stream = listOfTokens.stream()) {
+			tokenToCountMap = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		} finally {
+			// Allow this probably huge list to be garbage collected
+			listOfTokens = null;
+		}
 
+		LOGGER.debug("Size of map key set: {}", tokenToCountMap.keySet().size());
+		List<Map.Entry<String,Long>> mapEntryList = new ArrayList<>(tokenToCountMap.size());
+		for (Map.Entry<String,Long> entry : tokenToCountMap.entrySet()) {
+			mapEntryList.add(entry);
+		}
+		Comparator<Map.Entry<String,Long>> comparator = new Comparator<Map.Entry<String,Long>>() {
+			@Override public int compare(Entry<String, Long> a, Entry<String, Long> b) {
+				if (a.getValue() < b.getValue()) {
+					return -1;
+				} else if (a.getValue() > b.getValue()) {
+					return 1;
+				}
+				return 0;
+			}
+		};
+		Collections.sort(mapEntryList, comparator);
+		for (Map.Entry<String,Long> entry : mapEntryList) {
+			LOGGER.debug(String.format("%04d|%s",entry.getValue(),entry.getKey()));
+		}
+	}
 }
